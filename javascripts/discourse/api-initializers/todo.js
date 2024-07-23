44ba46e4b17a713749749919fe6e9781052fb194
DaVania <github_ivi@miransky.dk>
2024-07-23 21:56:56 +0200

    Initial commit

---
+++ javascripts/discourse/api-initializers/todo.js
@@ -0,0 +1,8 @@
+import { apiInitializer } from "discourse/lib/api";
+import LogoutHeaderButton from "../components/logout-header-button";
+
+export default apiInitializer("1.28.0", (api) => {
+  if (api.getCurrentUser()) {
+    api.headerIcons.add("logout", LogoutHeaderButton, { before: "auth" });
+  }
+});
